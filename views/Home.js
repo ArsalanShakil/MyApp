0bb42e3c9145a24cc145a676db03dbc82c60524f
Arsalan <arsalan_cr7@yahoo.com>
2021-01-18 16:28:10 +0500

    navigation task complete

---
+++ views/Home.js
@@ -0,0 +1,21 @@
+import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import List from '../components/List';
+import GlobalStyles from '../utils/GlobalStyles';
+import PropTypes from 'prop-types';
+
+const Home = ({navigation}) => {
+  return (
+    <SafeAreaView style={GlobalStyles.droidSafeArea}>
+      <List navigation={navigation} />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+};
+
+Home.propTypes = {
+  navigation: PropTypes.object,
+};
+
+export default Home;
